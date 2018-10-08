044652c8e3c92c963c71b07be0db2e5a8854bd08
alive <alive2212@gmail.com>
2018-10-08 12:32:53 +0330

    change somthing for pakcgist

--- config/laravel-echarge-service.php
+++ config/laravel-echarge-service.php
@@ -1,17 +1,24 @@
 <?php
 
 return [
-//    'merchant_id' => null,
-//    'url' => 'https://api.echargeservice.ir/Services/Strict/Version4/EchargeServiceV4.svc?wsdl',
-//    'ns' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
-
+    'username' => '',
+    'password' => '',
+    'merchant_id' => 0,
+//    'url' => 'https://api.echargeservices.com/Services/Strict/Version4/EchargeServiceV4.svc?wsdl',
+    'url' => 'https://api.echargeservice.ir/services/Strict/Version3/EchargeService.svc?wsdl',
+    'ns' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+    'soap' => [
+        'stream_context' => [
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ],
+        'options' => [
+            'trace' => true,
+            'exceptions' => true,
+            'cache_wsdl' => 0,
+        ],
+    ],
 ];
-
-//    'voucher' => [
-//        'url' => "https://api.echargeservice.ir/services/Strict/Version3/EchargeService.svc"
-//    ],
-//
-//    'top_up' => [
-//        'url' => "https://api.echargeservice.ir/Services/Strict/Version4/EchargeServiceV4.svc"
-//    ],
-//];
